df61260ea63348392d574b7c1fcce8688c79f4dc
Rafael <69431917+rafaelleitedasilva@users.noreply.github.com>
2022-10-21 11:04:02 -0300

    principalis

--- project/app/server.py
+++ project/app/server.py
@@ -11,6 +11,10 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
     
 @app.route('/posts')
 def posts():
