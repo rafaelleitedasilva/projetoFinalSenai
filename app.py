676e72f600a0969e0e5202718a2b328e80315a51
Rafael <102087586+PTorini1@users.noreply.github.com>
2022-11-04 08:26:13 -0300

    AjusteHeroku

---
+++ app.py
@@ -0,0 +1,95 @@
+import functools
+from flask import Flask,render_template, request
+#from flask_mysqldb import MySQL
+from flask import Flask
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+
+app = Flask(__name__)
+# - criando a conexao com o banco
+# mysql = MySQL(app)
+# app.config['MYSQL_HOST'] = 'localhost'
+# app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_PASSWORD'] = ''
+# app.config['MYSQL_DB'] = 'eductech'
+
+# -- routes
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+# @app.route('/teste_db') # , methods = ['POST', 'GET']
+# def teste():
+#     cur = mysql.connection.cursor()
+#     cur.execute('Select * from eductech.teste_post')
+#     fetchdata = cur.fetchall()
+#     cur.close()
+#     return render_template('teste_db.html', data= fetchdata)
+
+# @app.route('/login', methods = ['POST', 'GET'])
+# def login_screen():
+#     if request.method == 'POST':
+
+#         email = request.form['email']
+#         senha = request.form['senha']
+        
+#         cursor= mysql.connection.cursor()
+#         cursor.execute("SELECT * from eductech.teste_post WHERE nome_teste = '{}' AND senha_teste = '{}'".format(email, senha))
+#         dados = cursor.fetchone()
+#         print(dados)
+#         if dados:
+#             return home()
+#         else:
+#             msg = 'erro' 
+#             return render_template('login.html', data=msg)
+        # cursor_senha = mysql.connection.cursor_senha()
+        # cursor_senha.execute("SELECT * from eductech.teste_post WHERE senha_teste= '{}'".format(senha))
+        # yfetch = cursor_senha.fetchall()
+
+        
+        # if xfetch.rowcount ==1 : 
+        #     if senha != '' and QtdSenha[0][0] == senha:
+        #         # loginScreen.returnPressed.connect(funcao_principal())
+        #         funcao_principal() 
+        #         loginScreen.close()
+        #     else:
+        #         loginScreen.label_4.setText("Senha Incorreta")
+        # else:
+        #     loginScreen.label_4.setText("Email ou Nome de Usuário não encontrado")
+
+        # mysql.connection.commit()
+        # cursor_email.close()
+
+        # return render_template('login.html', data=xfetch)
+        # return render_template('home.html')
+
+    
+
+    #return render_template('login.html')
+          
+
+
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
+    
+@app.route('/posts')
+def posts():
+    return render_template('posts.html')
+
+@app.route('/tarefas')
+def tarefas():
+    return render_template('tarefas.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+""" @bp.route('/register', methods=('GET', 'POST'))
+def register():
+    return render_template('auth/register.html') """
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Aluno\Documents\projetoFinalSenai\venv
+set VIRTUAL_ENV=C:\Users\Aluno\Documents\GitHub\projetoFinalSenai\project\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
