3804b2fc864fe5d5ae65a6aaaca5f52d780696da
PTorini1 <102087586+PTorini1@users.noreply.github.com>
2022-11-21 10:56:19 -0300

    dashboardChanges

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,5 +1,6 @@
 // Pega os dados que estão dentro da div no html
 const ctx = document.getElementById('myChart').getContext("2d");
+const ctx2 = document.getElementById('myChart2').getContext("2d");
 
 let delayed;
 
@@ -28,6 +29,19 @@ const data = {
         data:[211, 326, 358, 459, 111, 333, 543, 976, 345,],
         label:["Evolução do Aluno"],
         fill: true,
+        backgroundColor: gradient,
+        borderColor: "#000",
+        pointBackgroundcolor: "#000",
+        tension: 0.5
+    }]
+}
+
+const data2 = {
+    labels, 
+    datasets: [{
+        data:[111, 222, 333, 444, 555, 666, 777, 888, 999,],
+        label:["Teste maluco"],
+        fill: true, 
         backgroundColor: gradient,
         borderColor: "#000",
         pointBackgroundcolor: "#000",
@@ -68,7 +82,41 @@ const config = {
     }
 }
 
+const config2 = {
+    type:'bar',
+    data:data2,
+    options: {
+        radius: 5,
+        hitRadius: 30,
+        hoverRadius: 12,
+        responsive:true,
+        animation:{
+            onComplete: () => {
+                delayed = true;
+            },
+            delay: (context) => {
+                let delay = 0;
+                if(context.type === "data" && context.mode === "default" && !delayed){
+                    delay = context.dataIndex * 300 + context.datasetIndex * 100;
+                }
+                return delay;
+            },
+        },
+        // Teste para transformação de dados (aqui no caso foi de centesimo para milhao)
+        scales: {
+            y: {
+                ticks:{
+                    callback: function(value){
+                        return '$' + value + "m";
+                    }
+                }
+            }
+        }
+    }
+}
+
 // Constante que vai carregar todos os dados do gráfico 
 const myChart = new Chart(ctx, config);
+const myChart2 = new Chart(ctx2, config2);
 
 
